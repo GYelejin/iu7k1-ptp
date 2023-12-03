549d26e13e44587e672824d7e35b639ae85c9e66
root <root@1882975-nikitiy.twc1.net>
2023-12-03 20:36:48 +0300

    shfmt applied to scripts

--- test.sh
+++ test.sh
@@ -4,16 +4,15 @@
 count=5
 
 i=1
-while [ $i -le $count ]; do 
+while [ $i -le $count ]; do
 	echo "Test ${i}"
 
 	f1name="t${i}f1.txt"
 	f2name="t${i}f2.txt"
 
 	./comparator1.sh "tests/${f1name}" "tests/${f2name}" -v
-	
-	i=$(($i+1))
 
-	echo 
+	i=$((i + 1))
+
+	echo
 done
-
