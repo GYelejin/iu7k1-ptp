ff4a23c033bf22835ee9db2390829815237675ed
root <root@1882975-nikitiy.twc1.net>
2023-12-10 23:29:24 +0300

    shellcheck test.sh

--- test.sh
+++ test.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Count of tests
-count=7
-
+# Reight test results
 answers="0 0 0 1 1 0 0"
 
 i=1
 
+# Console color definitions
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 NC="\033[0m"
 
-for ans in $answers ; do
+# Iterate over tests
+for ans in $answers; do
 	echo "Test ${i}"
 
 	f1name="t${i}f1.txt"
 	f2name="t${i}f2.txt"
 
 	./comparator1.sh "tests/${f1name}" "tests/${f2name}" -v
-	
-	code=$?
-	if [ $ans -eq $code ] ; then 
+
+	# Check the exit code of executed script
+	if [ "$ans" -eq $? ]; then
 		echo -e "Result: ${GREEN}PASSED${NC}"
 	else
 		echo -e "Result: ${RED}FAILED${NC}"
